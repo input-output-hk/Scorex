dd2d2c1e336b5640eff37782ab24389caaf5aeb1
kushti <kushtech@yahoo.com>
2016-03-01 09:44:01 +0300

    new structures WIP0

--- scorex-basics/src/main/scala/scorex/transaction/Transaction.scala
+++ scorex-basics/src/main/scala/scorex/transaction/Transaction.scala
@@ -18,6 +18,54 @@ trait Transaction extends StateChangeReason {
     * A transaction could be serialized into JSON
     */
   def json: JsObject
+}
 
 
+/**
+  * New structures
+  */
+
+
+//todo: state events, any crypto, ZKPs,
+
+sealed trait Lock
+
+case object NoLock extends Lock
+
+case class ToPreimage(hash: Array[Byte]) extends Lock
+
+case class ToPubKey(pubKey: Array[Byte]) extends Lock
+
+case class And(lock1: Lock, Lock2: Lock) extends Lock
+
+/**
+  * Box is a state element. Basically it is some value locked by some state machine.
+  */
+trait Box {
+  val locks: Seq[Lock]
+
+  val memo: Array[Byte]
+  val value: Long
+}
+
+/**
+  * todo: Modifier
+  */
+
+trait BoxModifier {
+  val key: Any
+}
+
+/**
+  *
+  * Transaction changes state of existing boxes and creates new ones
+  */
+trait NewTransaction {
+  val fee: Long
+
+  val timestamp: Long
+
+  val modifiers: Seq[BoxModifier]
+
+  val newBoxes: Seq[Box]
 }
