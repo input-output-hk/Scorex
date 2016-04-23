0597a865d9a972cf9e61fccb6905b0c346074872
kushti <kushtech@yahoo.com>
2016-04-23 16:55:20 +0300

    AndSeq

--- scorex-basics/src/main/scala/scorex/transaction/StateChangeReason.scala
+++ scorex-basics/src/main/scala/scorex/transaction/StateChangeReason.scala
@@ -1,5 +1,7 @@
 package scorex.transaction
 
+import com.google.common.primitives.Ints
+
 
 /**
   * A proof
@@ -9,8 +11,15 @@ trait Proof {
   val bytes: Array[Byte]
 }
 
-case class Signature(signature: Array[Byte]) extends Proof{
-  override val bytes: Array[Byte] = Array()
+case class Signature(signature: Array[Byte]) extends Proof {
+  override lazy val bytes: Array[Byte] = signature
+}
+
+case class AndSeq(proofs: Seq[Proof]) extends Proof {
+  override lazy val bytes =
+    proofs.foldLeft(Ints.toByteArray(proofs.size)) { case (bs, proof) =>
+      bs ++ Ints.toByteArray(proof.bytes.length) ++ proof.bytes
+    }
 }
 
 case object NoProof extends Proof {
