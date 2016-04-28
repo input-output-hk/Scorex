d25ed456e4b907c3de8378bdd38dad0dec452c9e
gagarin55 <gagarin55@protonmail.com>
2016-04-28 02:23:17 -0700

    minor refinement for test

--- scorex-transaction/src/test/scala/scorex/transaction/state/wallet/PaymentUnitTests.scala
+++ scorex-transaction/src/test/scala/scorex/transaction/state/wallet/PaymentUnitTests.scala
@@ -1,14 +1,13 @@
 package scorex.transaction.state.wallet
 
 import org.scalatest._
-import play.api.libs.json.{JsError, JsSuccess, Json}
+import play.api.libs.json.Json
 
 class PaymentUnitTests extends FunSuite {
 
   test("Parsing payment json without sender field returns JsError") {
     val body = "{\"amount\": 100,\n\"fee\": 1,\n\"recipient\": \"recipient\"}"
     val jsValue = Json.parse(body);
-    println(jsValue.validate[Payment].toString)
     assert(jsValue.validate[Payment].isError)
   }
 }
