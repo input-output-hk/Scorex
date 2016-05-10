3cf3cb8bd98cb320c0ecf937e9b958c18cd234c5
kushti <kushtech@yahoo.com>
2016-05-10 16:23:36 +0300

    Proof comments

--- scorex-basics/src/main/scala/scorex/transaction/proof/Proof.scala
+++ scorex-basics/src/main/scala/scorex/transaction/proof/Proof.scala
@@ -1,5 +1,14 @@
 package scorex.transaction.proof
 
+/**
+  * The most general abstraction of fact a prover can provide a non-interactive proof
+  * to open a box or to modify an account
+  */
+
 trait Proof {
+
+  /**
+    * The proof is non-interactive and thus serializable
+    */
   val bytes: Array[Byte]
 }
