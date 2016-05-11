8c0ccc145ce9b188b41f844919fbeb7ddb7bd6c2
kushti <kushtech@yahoo.com>
2016-05-11 13:19:29 +0300

    boxIsClosed() comment, formatting

--- scorex-basics/src/main/scala/scorex/transaction/state/BoxMinimalState.scala
+++ scorex-basics/src/main/scala/scorex/transaction/state/BoxMinimalState.scala
@@ -2,15 +2,18 @@ package scorex.transaction.state
 
 import scorex.block.Block
 import scorex.transaction.Transaction
-import scorex.transaction.box.{Proposition, Box}
+import scorex.transaction.box.{Box, Proposition}
 
 import scala.util.Try
 
 trait BoxMinimalState[L <: Proposition] extends MinimalState[Box[L]] {
 
-  //the only question minimal box state could answer is "whether a box is closed?".
-  //please note the answer is "no" even if a box never existed
-  def boxIsClosed(boxId: Box[L]#Id):Boolean
+  /**
+    * The only question minimal box state could answer is "whether a box is closed?",
+    * as it stores closed boxes only
+    * Please note the answer is "no" even if a box never existed
+    */
+  def boxIsClosed(boxId: Box[L]#Id): Boolean
 
   override private[transaction] def processBlock(block: Block): Try[BoxMinimalState[L]]
 
