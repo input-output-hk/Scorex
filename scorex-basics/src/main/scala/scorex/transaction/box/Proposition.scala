00d4301a930fc38c38b1af527c49bcb2acdcbf39
kushti <alex.chepurnoy@iohk.io>
2016-05-19 13:18:31 +0300

    typo fix

--- scorex-basics/src/main/scala/scorex/transaction/box/Proposition.scala
+++ scorex-basics/src/main/scala/scorex/transaction/box/Proposition.scala
@@ -5,18 +5,18 @@ import com.google.common.primitives.Ints
 sealed trait Proposition {
   val encodedType: Byte
 
-  protected val speficicBytes: Array[Byte]
+  protected val specificBytes: Array[Byte]
 
-  final val bytes: Array[Byte] = Array(encodedType) ++ speficicBytes
+  final val bytes: Array[Byte] = Array(encodedType) ++ specificBytes
 }
 
 sealed trait SigmaProposition extends Proposition {
   val x: Array[Byte]
 
-  override val speficicBytes = x
+  override val specificBytes = x
 }
 
 case class HeightOpenProposition(height: Int) extends Proposition {
   override val encodedType = 0: Byte
-  override val speficicBytes = Ints.toByteArray(height)
+  override val specificBytes = Ints.toByteArray(height)
 }
