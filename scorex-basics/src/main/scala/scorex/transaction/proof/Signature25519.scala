2c1c4933c6a7bbcf7d65dda5c099edca63571d00
kushti <alex.chepurnoy@iohk.io>
2016-05-21 23:00:24 +0300

    Signature25519 comments

--- scorex-basics/src/main/scala/scorex/transaction/proof/Signature25519.scala
+++ scorex-basics/src/main/scala/scorex/transaction/proof/Signature25519.scala
@@ -3,9 +3,6 @@ package scorex.transaction.proof
 import scorex.crypto.signatures.Curve25519
 
 /**
-  * Special case to not rewrite a lot of code in SimnplestTransactionModule
-  * Please note byte serialization is different from all other proofs
-  * So it could not be used in conjuctions with other proofs
   * @param signature 25519 signature
   */
 case class Signature25519(signature: Array[Byte]) extends Proof {
@@ -17,7 +14,3 @@ case class Signature25519(signature: Array[Byte]) extends Proof {
 object Signature25519 {
   lazy val SignatureSize = Curve25519.SignatureLength25519
 }
-
-
-
-
