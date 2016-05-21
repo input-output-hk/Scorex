f6f00d90b52a06c4eda6c60822430581bc512a42
kushti <alex.chepurnoy@iohk.io>
2016-05-21 21:48:48 +0300

    imports fix in utils

--- scorex-basics/src/main/scala/scorex/utils/utils.scala
+++ scorex-basics/src/main/scala/scorex/utils/utils.scala
@@ -4,27 +4,27 @@ import java.security.SecureRandom
 
 import scala.annotation.tailrec
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 package object utils {
 
   @tailrec
   final def untilTimeout[T](timeout: FiniteDuration,
                             delay: FiniteDuration = 100.milliseconds)(fn: => T): T = {
-    util.Try {
+    Try {
       fn
     } match {
-      case util.Success(x) => x
+      case Success(x) => x
       case _ if timeout > delay =>
         Thread.sleep(delay.toMillis)
         untilTimeout(timeout - delay, delay)(fn)
-      case util.Failure(e) => throw e
+      case Failure(e) => throw e
     }
   }
 
-  def randomBytes(howMany: Int = 32):Array[Byte] = {
+  def randomBytes(howMany: Int = 32): Array[Byte] = {
     val r = new Array[Byte](howMany)
     new SecureRandom().nextBytes(r) //overrides r
     r
   }
-
 }
